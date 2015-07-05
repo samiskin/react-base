a8134ebffd131e55efc747295309d6dec1639a01
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-07-04 22:41:02 -0400

    testing hook

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 var HelloWorld = React.createClass({
   render() {
     return (
-      <div> Hello World! </div>
+      <div> Hello from planet earth! </div>
     );
   }
 });
