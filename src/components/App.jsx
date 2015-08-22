f2aa8f4308facb9780ef1f188cb90b1dfa5783c7
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-08-22 00:51:53 -0400

    Added NotFoundRoute and made a general App component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+
+var Component = require('Component');
+var HelloWorld = require('HelloWorld.jsx');
+
+export default class App extends Component{
+
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+
+
+
+}
+
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 
-var HelloWorld = require('HelloWorld.jsx');
+var App = require('App.jsx');
 
 var Router = require('react-router');
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var routes = (
-    <Route path={"/"} handler={HelloWorld}>
+    <Route path={"/"} handler={App}>
+      <NotFoundRoute handler={App}/>
     </Route>
 );
 
