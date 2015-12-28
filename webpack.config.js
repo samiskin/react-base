d0ed83abe8b198d93f3da1fbdf80a0fdc6e922d9
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-12-27 22:50:43 -0500

    Used jerrysus webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,78 @@
-'use strict';
+// Based off of https://github.com/jerrysu/webpack-boilerplate/blob/master/webpack.config.dev.js
 
-const path = require('path');
-const webpack = require('webpack');
-// var pkg = require('./package.json');
-const autoprefixer = require('autoprefixer');
-const precss = require('precss');
-const postcssImport = require('postcss-import');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    'webpack-hot-middleware/client',
     './src/app'
   ],
 
+  devtool: 'cheap-module-eval-source-map',
+
   output: {
-    path: __dirname,
-    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
 
   resolve: {
     root: path.join(__dirname, 'src'),
-    modulesDirectories: ['node_modules', 'components', 'src', 'lib'],
-    extensions: ['', '.js'],
-  },
-
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
-  postcss: function(webpackDependency) {
-    return [
-      postcssImport({
-        addDependencyTo: webpackDependency,
-        path: '/'
-      }),
-      autoprefixer,
-      precss
-    ];
+    modulesDirectories: ['node_modules', 'src/components', 'src', 'src/lib'],
+    extensions: ['', '.js']
   },
 
   module: {
     loaders: [
-      {test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/},
-      {test: /\.json$/, loader: 'json'},
-      {test: /\.css$/, loaders: ['style', 'css?modules', 'postcss']}
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss'
+        ],
+        exclude: /node_modules/
+      },
+      {test: /\.css$/, loaders: ['style', 'css', 'postcss'], include: /node_modules/},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.gif/, loader: 'url-loader?limit=10000&mimetype=image/gif'},
+      {test: /\.jpg/, loader: 'url-loader?limit=10000&mimetype=image/jpg'},
+      {test: /\.png/, loader: 'url-loader?limit=10000&mimetype=image/png'}
     ]
   },
 
-  devtool: '#inline-source-map'
+  plugins: [
+    new webpack.ProvidePlugin({
+      'React': 'react'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      },
+      __DEV__: process.env.NODE_ENV === 'development'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
 
+  postcss: [
+    require('autoprefixer'),
+    require('postcss-color-rgba-fallback'),
+    require('postcss-focus'),
+    require('postcss-hexrgba'),
+    require('postcss-opacity'),
+    require('postcss-nested')
+  ]
 };
